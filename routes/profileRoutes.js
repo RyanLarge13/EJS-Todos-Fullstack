c539d68f25dbe0c072142a9b8e467f0b2b952512
RyanLarge13 <ryanhudsonlarge13@gmail.com>
2022-10-18 19:24:30 -0700

    Restarting the authintication and routes.

--- routes/profileRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import {
-    renderProfile,
-} from "../controllers/profile.js";
-
-export const profileRouter = express.Router();
-
-//profileRouter will handle all backend logic for handling the display, manipulation, creation, and deleting a user's todos.
-profileRouter.route("/profile").get(renderProfile);
--- server.js
+++ server.js
@@ -2,8 +2,6 @@ import express from "express";
 import dotenv from "dotenv";
 import parser from "body-parser";
 import { connectDB } from "./config/db.js";
-import { loginRouter } from "./routes/loginRoutes.js";
-import { profileRouter } from "./routes/profileRoutes.js";
 dotenv.config();
 connectDB();
 
@@ -13,13 +11,20 @@ const app = express();
 //Middleware
 app.use(parser.urlencoded({ extended: true }));
 app.use(parser.json());
-app.use(loginRouter, profileRouter);
 app.use(express.static("views"));
 app.set("view engine", "ejs");
 
 //Incorporating the get request for the applications main index.ejs file from the server js.
 app.get("/", (req, res) => {
-  res.status(200).render("index");
+  const user = req.user;
+  if (user) {
+    res.status(200).render("index", {
+      welcome: `Welcome back ${user.Username}`,
+    });
+  }
+  else {
+    res.status(200).render('index');
+  }
 });
 
 app.listen(port, () => console.log(`Your app is listening on port ${port} : http://localhost:8080`));
